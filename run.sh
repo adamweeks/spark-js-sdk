95efabd794585a6ff15b05a897a732d7a2475dbd
Ian W. Remmel <design@ianwremmel.com>
2016-08-15 14:27:54 -0700

    #no-push

--- run.sh
+++ run.sh
@@ -7,7 +7,7 @@ ps aux | grep grunt | grep -v grunt | awk '{print $2}' | xargs kill > /dev/null 
 
 # Now, fail if anything fails
 set -e
-
+set -x
 
 #
 # CONFIGURE NODE
@@ -92,12 +92,12 @@ if [ -n "${ENABLE_VERBOSE_NETWORK_LOGGING}" ]; then
   DOCKER_RUN_ENV+=" -e ENABLE_VERBOSE_NETWORK_LOGGING=${ENABLE_VERBOSE_NETWORK_LOGGING} "
 fi
 export DOCKER_CONTAINER_NAME="${JOB_NAME}-builder"
-export DOCKER_RUN_OPTS="${DOCKER_RUN_ENV} --rm --volumes-from ${HOSTNMAME} ${DOCKER_CONTAINER_NAME}"
 
 echo "WORKDIR ${WORKDIR}" >> ./docker/builder/Dockerfile
 docker build -t ${DOCKER_CONTAINER_NAME} ./docker/builder
 git checkout ./docker/builder/Dockerfile
 
+export DOCKER_RUN_OPTS="${DOCKER_RUN_ENV} -e NPM_CONFIG_CACHE=${WORKDIR}/.npm  --rm --volumes-from ${HOSTNAME} --user=$(id -u):$(id -g) ${DOCKER_CONTAINER_NAME}"
 
 #
 # MAKE SECRETS AVAILABLE TO AUX CONTAINERS
