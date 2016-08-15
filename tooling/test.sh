95a2f9669faec8f37d1eeeb9e7ab500220c94575
Ian W. Remmel <design@ianwremmel.com>
2016-08-15 11:40:17 -0700

    #no-push

--- tooling/test.sh
+++ tooling/test.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 set -e
+set -x
 
 START_DIR=$(pwd)
 cd "${START_DIR}/../builder"
@@ -40,9 +41,11 @@ if [ -n "${ENABLE_VERBOSE_NETWORK_LOGGING}" ]; then
   DOCKER_RUN_ENV+=" -e ENABLE_VERBOSE_NETWORK_LOGGING=${ENABLE_VERBOSE_NETWORK_LOGGING} "
 fi
 
-DOCKER_RUN_OPTS="${DOCKER_RUN_ENV} -it --rm -v $(pwd):/workspace spark-js-sdk-builder"
+DOCKER_RUN_OPTS="${DOCKER_RUN_ENV} --rm -v $(pwd):/workspace spark-js-sdk-builder"
 
 echo "INSTALLING LEGACY DEPENDENCIES"
+docker run ${DOCKER_RUN_OPTS} pwd
+docker run ${DOCKER_RUN_OPTS} ls
 docker run ${DOCKER_RUN_OPTS} npm install
 
 echo "CLEANING"
