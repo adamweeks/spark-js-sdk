67f4db2137577f94c8e95a2e7d4b8f93c4c75ab3
Ian W. Remmel <design@ianwremmel.com>
2016-08-19 10:56:56 -0700

    refactor(tooling): replaces spaces before gzipping

--- tooling/archive.sh
+++ tooling/archive.sh
@@ -5,5 +5,12 @@ set -e
 cd "${SDK_ROOT_DIR}"
 
 find ./reports -type f | xargs ls -lh | awk '{print $5 "," $6 " " $7 " " $8 "," $9}' > artifacts.csv
+
+# This little nugget of bash magic should replace all spaces in filenames with
+# underscores
+# see http://unix.stackexchange.com/questions/223182/how-to-replace-spaces-in-all-file-names-with-underscore-in-linux-using-shell-scr
+find ./reports -name "* *" -print0 | sort -rz | \
+  while read -d $'\0' f; do mv -v "$f" "$(dirname "$f")/$(basename "${f// /_}")"; done
+
 gzip -r ./reports
 mv artifacts.csv ./reports/artifacts.csv
