767799696d7282aeb89e0c8a5bcc5ed63f765fc3
Ian W. Remmel <design@ianwremmel.com>
2016-08-23 16:01:57 -0700

    chore(tooling): print package versions in each build

--- tooling/test.sh
+++ tooling/test.sh
@@ -30,6 +30,13 @@ echo "# BOOTSTRAPPING MODULES"
 echo "################################################################################"
 docker run ${DOCKER_RUN_OPTS} npm run bootstrap
 
+set +e
+echo "# Top Level Dependencies"
+npm ls --depth 0
+echo "# Package Dependencies"
+npm run lerna -- exec -- npm ls --depth 0
+set -e
+
 echo "################################################################################"
 echo "# BUILDING MODULES"
 echo "################################################################################"
